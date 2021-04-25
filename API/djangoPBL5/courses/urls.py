14c20006bd01497e62179d218ad0ec7ec6d05e56
nguyenvu19 <nguyenvu191000@gmail.com>
2021-04-25 22:02:09 +0700

    api

---
+++ API/djangoPBL5/courses/urls.py
@@ -0,0 +1,10 @@
+from .views import RegisterAPI, LoginAPI
+from django.urls import path
+from knox import views as knox_views
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+]
