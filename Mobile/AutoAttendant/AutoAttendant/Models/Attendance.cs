76660f594b78f06bd572189654538692a91dfae8
minh2605 <bluewings2605@gmail.com>
2021-05-09 15:04:57 +0700

    update

---
+++ Mobile/AutoAttendant/AutoAttendant/Models/Attendance.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoAttendant.Models
+{
+    public class Attendance
+    {
+        private string idStudent;
+        private string idSubject;
+        private bool state;
+        private string faceImg;
+        private DateTime time;
+
+        public string IdStudent
+        {
+            get
+            {
+                return this.idStudent;
+            }
+            set
+            {
+                this.idStudent = value;
+            }
+        }
+        public string IdSubject
+        {
+            get
+            {
+                return this.idSubject;
+            }
+            set
+            {
+                this.idSubject = value;
+            }
+        }
+        public bool State
+        {
+            get
+            {
+                return this.state;
+            }
+            set
+            {
+                this.state = value;
+            }
+        }
+        public string FaceImg
+        {
+            get
+            {
+                return this.faceImg;
+            }
+            set
+            {
+                this.faceImg = value;
+            }
+        }
+        public DateTime Time
+        {
+            get
+            {
+                return this.time;
+            }
+            set
+            {
+                this.time = value;
+            }
+        }
+
+        public Attendance()
+        {
+
+        }
+
+        public Attendance(string idStudent, string idSubject, bool state, string faceImg, DateTime time)
+        {
+            this.IdStudent = idStudent;
+            this.IdSubject = idSubject;
+            this.State = state;
+            this.FaceImg = faceImg;
+            this.Time = time;
+        }
+    }
+
+    
+}
---
+++ Mobile/AutoAttendant/AutoAttendant/Models/Lecture.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoAttendant.Models
+{
+    public class Lecture
+    {
+        private string id;
+        private string name; 
+        private string age;
+        private string phone;
+        private string faculty;
+
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return this.phone;
+            }
+            set
+            {
+                this.phone = value;
+            }
+        }
+
+        public string Faculty
+        {
+            get
+            {
+                return this.faculty;
+            }
+            set
+            {
+                this.faculty = value;
+            }
+        }
+
+        public Lecture()
+        {
+
+        }
+
+        public Lecture(string id, string name, string age, string phone, string faculty)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Phone = phone;
+            this.Faculty = faculty;
+        }
+    }
+}
